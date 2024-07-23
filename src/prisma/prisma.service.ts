63a057053c24d1838045ad3a35f7333a2498d3b9
Lucas Antunes <lucasssantunes@hotmail.com>
2024-07-23 18:53:01 -0300

    Add prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super()
+  }
+
+  onModuleInit() {
+    return this.$connect
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect
+  }
+}
